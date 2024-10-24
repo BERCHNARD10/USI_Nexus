7158dc51175e1afa9793b138fedd9ba7314cbe28
BERCHNARD10 <20210643@uthh.edu.mx>
2024-10-24 16:16:41 -0600

    Actualizacion del package.json intento 4

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     testEnvironment: 'jsdom', // Esto es importante para pruebas en React
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    transform: {
+      "^.+\\.jsx?$": ["babel-jest", { sourceType: "module" }]
+    },
   };
   
