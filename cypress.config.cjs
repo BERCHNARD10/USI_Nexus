bda4253e9b8b69f6714d54590abe84bf2d69e0d2
BERCHNARD10 <20210643@uthh.edu.mx>
2024-10-25 09:21:14 -0600

    Actualizacion del WorkFlow v3

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,9 +1,10 @@
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
-      // implementar eventos si es necesario
+      // implementa los eventos de nodo aquí si es necesario
     },
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    supportFile: false,  // Desactivar archivo de soporte
   },
 };
