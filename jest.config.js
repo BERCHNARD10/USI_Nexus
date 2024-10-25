67d7f70328f65c2a2775bd6b69ccc95f9f1d155d
BERCHNARD10 <20210643@uthh.edu.mx>
2024-10-25 16:16:51 -0600

    Eliminar archivos solo en remoto

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    testEnvironment: 'jsdom', // Esto es importante para pruebas en React
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    transform: {
-      "^.+\\.jsx?$": ["babel-jest", { sourceType: "module" }]
-    },
-  };
-  
