c3a3744c8cb84df2614ee9d86a257eedb3f3ac83
BERCHNARD10 <20210643@uthh.edu.mx>
2024-10-25 09:06:08 -0600

    Actualizacion del WorkFlow v2

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+// cypress.config.js
+module.exports = {
+    e2e: {
+      setupNodeEvents(on, config) {
+        // implementa eventos node si es necesario
+      },
+      baseUrl: 'http://localhost:3000', // URL de tu app en desarrollo
+    },
+  };
+  
