a17d824e3134f87157d315032102c3afe6128126
BERCHNARD10 <20210643@uthh.edu.mx>
2024-10-25 10:02:11 -0600

    Actualizacion del WorkFlow v5

--- cypress/e2e/sample_test.js
+++ cypress/e2e/sample_test.js
@@ -1,7 +1,7 @@
 describe('Sample Test', () => {
     it('Visita la página principal', () => {
       cy.visit('/');
-      cy.contains('Your App Title'); // Ajusta el contenido a algo que esté en tu aplicación.
+      cy.contains('Para los Alumnos'); // Este es el texto que debería estar en tu página
     });
   });
   
