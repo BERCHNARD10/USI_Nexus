90c92d4691d0ad825f187d7a4cb5ae6d4ec77041
BERCHNARD10 <20210643@uthh.edu.mx>
2024-10-28 15:25:09 -0600

    Elimina todos los archivos del repositorio remoto

--- cypress/e2e/sample_test.js
+++
@@ -1,7 +0,0 @@
-describe('Sample Test', () => {
-    it('Visita la página principal', () => {
-      cy.visit('/');
-      cy.contains('Para los Alumnos'); // Este es el texto que debería estar en tu página
-    });
-  });
-  
