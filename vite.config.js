49ba296600f76bf9b573b600a78c9cef76117f11
BERCHNARD10 <20210643@uthh.edu.mx>
2024-11-11 15:35:22 -0600

    UPDATE VITE.CONFIG

--- vite.config.js
+++ vite.config.js
@@ -1,77 +1,70 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
-// https://vitejs.dev/config/
-/*export default defineConfig({
-  plugins: [react()],
-})*/
+
 const manifestForPlugin = {
-  manifest: {
-    name: "UTHH Virtual",
-    short_name: "UTHH Virtual",
-    start_url: "/",
-    display: "standalone",
-    background_color: "#ffffff",
-    lang: "en",
-    scope: "/",
-    registerType: "autoUpdate",
-    includeAssets: [
-      "favicon-196.ico",
-      "apple-icon-180.png",
-      "manifest-icon-192.maskable.png",
-      "manifest-icon-512.maskable.png",
-      "logo-pwa-resized-phone-192.png"
-  
-    ],
-    icons: [
-      {
-        src: "manifest-icon-144.png",
-        sizes: "144x144",
-        type: "image/png",
-        purpose: "any"
-      },
-      {
-        src: "logo-pwa-resized-phone-192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any"
-      },
-      {
-        src: "logo-pwa-resized-phone-192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "maskable"
-      },
-      {
-        src: "manifest-icon-512.maskable-pwa.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any"
-      },
-      {
-        src: "manifest-icon-512.maskable-pwa.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable"
-      }
-    ],
-    theme_color: "#171717",
-    orientation: "portrait",
-    screenshots: [
-      {
-        src: "screenshot-wide.png",
-        sizes: "1280x720",
-        type: "image/png",
-        form_factor: "wide"
-      },
-      {
-        src: "screenshot-narrow.png",
-        sizes: "720x1280",
-        type: "image/png",
-        form_factor: "narrow"
-      }
-    ]
-  }
+  name: "UTHH Virtual",
+  short_name: "UTHH Virtual",
+  start_url: "/",
+  display: "standalone",
+  background_color: "#ffffff",
+  lang: "en",
+  scope: "/",
+  includeAssets: [
+    "favicon-196.ico",
+    "apple-icon-180.png",
+    "manifest-icon-192.maskable.png",
+    "manifest-icon-512.maskable.png",
+    "logo-pwa-resized-phone-192.png"
+  ],
+  icons: [
+    {
+      src: "manifest-icon-144.png",
+      sizes: "144x144",
+      type: "image/png",
+      purpose: "any"
+    },
+    {
+      src: "logo-pwa-resized-phone-192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "any"
+    },
+    {
+      src: "logo-pwa-resized-phone-192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "maskable"
+    },
+    {
+      src: "manifest-icon-512.maskable-pwa.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "any"
+    },
+    {
+      src: "manifest-icon-512.maskable-pwa.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "maskable"
+    }
+  ],
+  theme_color: "#171717",
+  orientation: "portrait",
+  screenshots: [
+    {
+      src: "screenshot-wide.png",
+      sizes: "1280x720",
+      type: "image/png",
+      form_factor: "wide"
+    },
+    {
+      src: "screenshot-narrow.png",
+      sizes: "720x1280",
+      type: "image/png",
+      form_factor: "narrow"
+    }
+  ]
 };
 
 export default defineConfig({
@@ -79,83 +72,79 @@ export default defineConfig({
     react(),
     VitePWA({
       manifest: manifestForPlugin,
-      cleanupOutdatedCaches: true,  // Esto limpiará automáticamente las cachés obsoletas
+      registerType: 'autoUpdate',
+      cleanupOutdatedCaches: true,
       workbox: {
         runtimeCaching: [
           {
-            urlPattern: ({ request }) => request.mode === 'navigate', // Caché para páginas
-            handler: 'NetworkFirst', // Intentar red, si falla usar caché
+            urlPattern: ({ request }) => request.mode === 'navigate',
+            handler: 'NetworkFirst',
             options: {
               cacheName: 'pages-cache',
               expiration: {
-                maxEntries: 15, // Máximo 10 páginas
-                maxAgeSeconds: 60 * 60 * 24 * 7, // Mantener en caché por 7 días
+                maxEntries: 15,
+                maxAgeSeconds: 60 * 60 * 24 * 7,
               },
             },
           },
           {
-            // Cache para imágenes (PNG, JPG, JPEG, SVG, GIF, WebP)
             urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
             handler: 'CacheFirst',
             options: {
               cacheName: 'image-cache',
               expiration: {
-                maxEntries: 50, // Limita el número de imágenes en el caché
-                maxAgeSeconds: 30 * 24 * 60 * 60, // Expira en 30 días
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
               },
             },
           },
           {
-            // Cache para assets estáticos (JS, CSS, imágenes)
             urlPattern: ({ request }) =>
               request.destination === 'script' || 
               request.destination === 'style' || 
-              request.destination === 'image', // Caché para JS, CSS, imágenes
-            handler: 'CacheFirst', // Usar caché primero, si no está, descargar
+              request.destination === 'image',
+            handler: 'CacheFirst',
             options: {
               cacheName: 'assets-cache',
               expiration: {
-                maxEntries: 50, // Máximo 50 activos estáticos
-                maxAgeSeconds: 60 * 60 * 24 * 30, // Mantener en caché por 30 días
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
               },
             },
           },
           {
-            // Cache para tus APIs (Web Services)
             urlPattern: /^https:\/\/robe\.host8b\.me\/WebServices\/.*\.php$/,
-            handler: 'NetworkFirst', // Intenta la red primero, si falla, busca en cache
+            handler: 'NetworkFirst',
             options: {
               cacheName: 'api-cache',
-              networkTimeoutSeconds: 10, // Tiempo de espera de la red antes de usar cache
+              networkTimeoutSeconds: 10,
               expiration: {
-                maxEntries: 20, // Máximo 20 respuestas de API
-                maxAgeSeconds: 60 * 60 * 24 * 9, // Cache de 7 días
+                maxEntries: 20,
+                maxAgeSeconds: 60 * 60 * 24 * 9,
               },
               cacheableResponse: {
-                statuses: [0, 200], // Cachea solo respuestas con código 0 (offline) o 200 (OK)
+                statuses: [0, 200],
               },
             },
           },
         ],
-        globDirectory: 'dist', // Donde están los archivos de compilación
+        globDirectory: 'dist',
         globPatterns: ['**/*.{html,js,css,png,jpg,svg}'],
-        cleanupOutdatedCaches: true, // Limpiar cachés antiguas
-        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024, // 10 MB
+        cleanupOutdatedCaches: true,
+        maximumFileSizeToCacheInBytes: 50 * 1024 * 1024,
       },
     }),
   ],
   server: {
-    host: true, // Esto debería permitir conexiones desde cualquier dirección IP,
+    host: true,
     mimeTypes: {
       'application/javascript': ['js', 'jsx']
     }
   },
   optimizeDeps: {
-    exclude: ['xlsx-style'] // Evitar que ciertos módulos sean pre-empacados
+    exclude: ['xlsx-style']
   },
   build: {
-    outDir: 'dist', // La carpeta de salida debe ser 'dist'
+    outDir: 'dist',
   }
-
 });
-
